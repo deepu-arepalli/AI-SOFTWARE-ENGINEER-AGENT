1d1a08fe268096ae4076a2a6e71252925a10f3d9
deepu-arepalli <deepikaarepalli217@gmail.com>
2026-08-17 20:17:26 +0530

    Initialize Spring Boot project

---
+++ src/main/java/com/deepika/ai_software_engineer_agent/AiSoftwareEngineerAgentApplication.java
@@ -0,0 +1,13 @@
+package com.deepika.ai_software_engineer_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSoftwareEngineerAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSoftwareEngineerAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepika/ai_software_engineer_agent/AiSoftwareEngineerAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepika.ai_software_engineer_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSoftwareEngineerAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
